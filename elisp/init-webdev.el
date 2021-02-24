bf1499663e537292d277f445e9caba0a7fe624df
fqye <fqye@daima.co>
2021-02-25 07:53:41 +0800

    updated init-webdev

--- elisp/init-webdev.el
+++ elisp/init-webdev.el
@@ -41,7 +41,7 @@
   (css-selector ((t (:inherit default :foreground "#66CCFF"))))
   (font-lock-comment-face ((t (:foreground "#828282"))))
   :mode
-  ("\\.phtml\\'" "\\.tpl\\.php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'"
+  ("\\.marko\\'" "\\.phtml\\'" "\\.tpl\\.php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'"
    "\\.erb\\'" "\\.mustache\\'" "\\.djhtml\\'" "\\.[t]?html?\\'"))
 ;; -WebModePac
 
