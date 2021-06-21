e15d0d48ef585757ac6930e62a263f3ab73d61e8
fqye <fqye@yizhanyun.cn>
2021-06-21 08:36:08 +0800

    Modified company

--- elisp/init-company.el
+++ elisp/init-company.el
@@ -80,56 +80,56 @@ If all failed, try to complete the common part with `company-complete-common'"
 ;; -ComPac
 
 ;; CompanyTabNinePac
-(use-package company-tabnine
-  :defer 1
-  :custom
-  (company-tabnine-max-num-results 9)
-  :bind
-  (("M-q" . company-other-backend)
-   ("C-z t" . company-tabnine))
-  :init
-  (defun company-tabnine-toggle (&optional enable)
-    "Enable/Disable TabNine. If ENABLE is non-nil, definitely enable it."
-    (interactive)
-    (if (or enable (not (memq 'company-tabnine company-backends)))
-        (progn
-          (add-hook 'lsp-after-open-hook #'lsp-after-open-tabnine)
-          (add-to-list 'company-backends #'company-tabnine)
-          (when (bound-and-true-p lsp-mode) (lsp-after-open-tabnine))
-          (message "TabNine enabled."))
-      (setq company-backends (delete 'company-tabnine company-backends))
-      (setq company-backends (delete '(company-capf :with company-tabnine :separate) company-backends))
-      (remove-hook 'lsp-after-open-hook #'lsp-after-open-tabnine)
-      (company-tabnine-kill-process)
-      (message "TabNine disabled.")))
-  (defun company//sort-by-tabnine (candidates)
-    "Integrate company-tabnine with lsp-mode"
-    (if (or (functionp company-backend)
-            (not (and (listp company-backend) (memq 'company-tabnine company-backends))))
-        candidates
-      (let ((candidates-table (make-hash-table :test #'equal))
-            candidates-lsp
-            candidates-tabnine)
-        (dolist (candidate candidates)
-          (if (eq (get-text-property 0 'company-backend candidate)
-                  'company-tabnine)
-              (unless (gethash candidate candidates-table)
-                (push candidate candidates-tabnine))
-            (push candidate candidates-lsp)
-            (puthash candidate t candidates-table)))
-        (setq candidates-lsp (nreverse candidates-lsp))
-        (setq candidates-tabnine (nreverse candidates-tabnine))
-        (nconc (seq-take candidates-tabnine 3)
-               (seq-take candidates-lsp 6)))))
-  (defun lsp-after-open-tabnine ()
-    "Hook to attach to `lsp-after-open'."
-    (setq-local company-tabnine-max-num-results 3)
-    (add-to-list 'company-transformers 'company//sort-by-tabnine t)
-    (add-to-list 'company-backends '(company-capf :with company-tabnine :separate)))
-  :hook
-  (kill-emacs . company-tabnine-kill-process)
-  :config
-  (company-tabnine-toggle t))
+;; (use-package company-tabnine
+;;   :defer 1
+;;   :custom
+;;   (company-tabnine-max-num-results 9)
+;;   :bind
+;;   (("M-q" . company-other-backend)
+;;    ("C-z t" . company-tabnine))
+;;   :init
+;;   (defun company-tabnine-toggle (&optional enable)
+;;     "Enable/Disable TabNine. If ENABLE is non-nil, definitely enable it."
+;;     (interactive)
+;;     (if (or enable (not (memq 'company-tabnine company-backends)))
+;;         (progn
+;;           (add-hook 'lsp-after-open-hook #'lsp-after-open-tabnine)
+;;           (add-to-list 'company-backends #'company-tabnine)
+;;           (when (bound-and-true-p lsp-mode) (lsp-after-open-tabnine))
+;;           (message "TabNine enabled."))
+;;       (setq company-backends (delete 'company-tabnine company-backends))
+;;       (setq company-backends (delete '(company-capf :with company-tabnine :separate) company-backends))
+;;       (remove-hook 'lsp-after-open-hook #'lsp-after-open-tabnine)
+;;       (company-tabnine-kill-process)
+;;       (message "TabNine disabled.")))
+;;   (defun company//sort-by-tabnine (candidates)
+;;     "Integrate company-tabnine with lsp-mode"
+;;     (if (or (functionp company-backend)
+;;             (not (and (listp company-backend) (memq 'company-tabnine company-backends))))
+;;         candidates
+;;       (let ((candidates-table (make-hash-table :test #'equal))
+;;             candidates-lsp
+;;             candidates-tabnine)
+;;         (dolist (candidate candidates)
+;;           (if (eq (get-text-property 0 'company-backend candidate)
+;;                   'company-tabnine)
+;;               (unless (gethash candidate candidates-table)
+;;                 (push candidate candidates-tabnine))
+;;             (push candidate candidates-lsp)
+;;             (puthash candidate t candidates-table)))
+;;         (setq candidates-lsp (nreverse candidates-lsp))
+;;         (setq candidates-tabnine (nreverse candidates-tabnine))
+;;         (nconc (seq-take candidates-tabnine 3)
+;;                (seq-take candidates-lsp 6)))))
+;;   (defun lsp-after-open-tabnine ()
+;;     "Hook to attach to `lsp-after-open'."
+;;     (setq-local company-tabnine-max-num-results 3)
+;;     (add-to-list 'company-transformers 'company//sort-by-tabnine t)
+;;     (add-to-list 'company-backends '(company-capf :with company-tabnine :separate)))
+;;   :hook
+;;   (kill-emacs . company-tabnine-kill-process)
+;;   :config
+;;   (company-tabnine-toggle t))
 ;; -CompanyTabNinePac
 
 ;; CompanyBoxPac
