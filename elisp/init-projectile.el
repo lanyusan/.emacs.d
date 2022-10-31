30ee9ac3f34d761bb4b0f5b3ad91b59cad8b799e
fqye <fqye@yizhanyun.cn>
2022-10-31 08:16:54 +0800

    tuning

--- elisp/init-projectile.el
+++ elisp/init-projectile.el
@@ -40,12 +40,15 @@
 
 ;; ProjPac
 (use-package projectile
-  :bind
-  ("C-c p" . projectile-command-map)
+  ;; :bind
+  ;; ("C-c p" . projectile-command-map)
   :custom
   (projectile-completion-system 'ivy)
   :config
   (projectile-mode 1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
   (when (and *sys/win32*
              (executable-find "tr"))
     (setq projectile-indexing-method 'alien))
